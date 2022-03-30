71ef1b5d6e189084084646e36b952310b0b8407f
SaiPurushoth <71919852+SaiPurushoth@users.noreply.github.com>
2022-03-30 13:09:13 +0530

    Add files via upload

---
+++ CoreJava/SpringJwt/src/main/java/com/example/app/controller/SpringContoller.java
@@ -0,0 +1,54 @@
+package com.example.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.app.models.AuthenticationRequest;
+import com.example.app.models.AuthenticationResponse;
+import com.example.app.service.MyUserDetailsService;
+import com.example.app.utils.JwtUtil;
+
+@RestController
+public class SpringContoller {
+    @Autowired
+	private MyUserDetailsService userDetailsService;
+	
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+    
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@RequestMapping("/")
+	public String hello() {
+		
+		return "hello world";
+	}
+	
+	@RequestMapping(value="/authenticate",method=RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+		try {
+		
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),authenticationRequest.getPassword()));
+	}
+		catch(BadCredentialsException e) {
+			throw new Exception("Incorrect username or password",e);
+		}
+		
+		
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+		
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+		
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
+	
+}
---
+++ CoreJava/SpringJwt/src/main/java/com/example/app/models/AuthenticationResponse.java
@@ -0,0 +1,19 @@
+package com.example.app.models;
+
+public class AuthenticationResponse {
+	
+	private final String jwt;
+
+	public String getJwt() {
+		return jwt;
+	}
+
+	public AuthenticationResponse(String jwt) {
+		super();
+		this.jwt = jwt;
+	}
+
+
+	
+
+}
